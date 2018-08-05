8aee293c08c40858aa5ca02d88f2627dcf1531f8
Emmanuel Bouchut <deepcraft@gmail.com>
2018-08-05 12:39:25 +0200

    creation des fichiers blades

--- app/resources/views/header_user.blade.php
+++ app/resources/views/header_user.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Hotelli</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -64,28 +64,3 @@
             }
         </style>
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-			<div class="top-right links">
-				@auth
-				<a href="{{ url('/home') }}">Home</a>
-				@else
-				<a href="{{ route('login') }}">Login</a>
-				<a href="{{ route('register') }}">Register</a>
-				@endauth
-			</div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="">Page d'accueil du site</a>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
