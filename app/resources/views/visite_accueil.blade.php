a1737b1dd8d87674f1af66cdbcab1abdbc8e36ab
Emmanuel Bouchut <deepcraft@gmail.com>
2018-08-05 15:56:40 +0200

    accueil hotelli

--- app/resources/views/visite_accueil.blade.php
+++ app/resources/views/visite_accueil.blade.php
@@ -11,72 +11,17 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-			<div class="top-right links">
-				@auth
-				<a href="{{ url('/home') }}">Home</a>
-				@else
-				<a href="{{ route('login') }}">Login</a>
-				<a href="{{ route('register') }}">Register</a>
-				@endauth
-			</div>
-            @endif
-
+        <div class="container">
+			<nav class="navbar navbar-light bg-light">
+				<a class="navbar-brand" href="#">
+				</a>
+				<ul>
+					<li><a href="/inscription">Inscription</a></li>
+				</ul>
+			</nav>
             <div class="content">
                 <div class="title m-b-md">
                     Hotelli
@@ -87,5 +32,9 @@
                 </div>
             </div>
         </div>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="{{ asset('js/bootstrap.min.js') }}"></script>
     </body>
 </html>
