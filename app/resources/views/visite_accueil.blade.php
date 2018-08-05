0923722a244480f9b05245827d7aa7795c2906af
Emmanuel Bouchut <deepcraft@gmail.com>
2018-08-05 13:12:02 +0200

    accueil hotelli

--- app/resources/views/visite_accueil.blade.php
+++ app/resources/views/visite_accueil.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hotelli
                 </div>
 
                 <div class="links">
