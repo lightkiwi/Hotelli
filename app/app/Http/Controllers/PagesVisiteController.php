514d9f1c9dfbc12d1cd410fcd74af1b7a57c9e14
Emmanuel Bouchut <deepcraft@gmail.com>
2018-08-29 17:59:56 +0200

    update du front visite

---
+++ app/app/Http/Controllers/PagesVisiteController.php
@@ -0,0 +1,81 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Http\Controllers;
+
+/**
+ * Description of PagesVisiteController
+ *
+ * @author deepgreen
+ */
+class PagesVisiteController
+{
+
+	/**
+	 * Page d'accueil
+	 * @return type
+	 */
+	public function index()
+	{
+		return view('pages.home');
+	}
+
+	public function faq()
+	{
+		return view('pages.faq');
+	}
+
+	public function cgu()
+	{
+		return view('pages.cgu');
+	}
+
+	public function contact()
+	{
+		return view('pages.contact');
+	}
+
+	/**
+	 * Gestion du login
+	 *
+	 * @param \App\Http\Controllers\Request $request
+	 * @return type
+	 */
+	public function login(Request $request)
+	{
+		$loginOk = [
+			'etat'	 => true,
+			'info'	 => 'Connexion réussie !'
+		];
+		$loginKo = [
+			'etat'	 => false,
+			'info'	 => 'Connexion échouée, merci de vérifier vos identifiants de connexion'
+		];
+		//vérification du login
+		if ($request->input('loginModalFormEmail') !== null && $request->input('loginModalFormPassword') !== null) {
+
+			$_SESSION['connected']		 = true;
+//			$_SESSION['profil'] = [];
+//			$_SESSION['profil']['date_login'] = date();
+			$_SESSION['profil']['id']	 = 3;
+			$_SESSION['profil']['email'] = $request->input('loginModalFormEmail');
+			$infoLogin					 = $loginOk;
+		} else {
+			$infoLogin = $loginKo;
+		}
+		//retour à la page d'accueil
+
+		return view('pages.home', compact('infoLogin'));
+	}
+
+	public function singin(Request $request)
+	{
+		$infoSignin = null;
+
+		return view('pages.home', compact('infoSignin'));
+	}
+}
--- app/resources/views/includes/visite/footer.blade.php
+++ app/resources/views/includes/visite/footer.blade.php
@@ -1,23 +1,8 @@
-<footer class="page-footer font-small cyan darken-3">
+<footer class="small bg-light">
 
-    <div class="container">
-		<div class="row">
-			<div class="col-md-12 py-5">
-				<div class="mb-5 flex-center">
-					<a class="fb-ic" href="/faq">
-						<i class="fa fa-address-book fa-lg white-text mr-md-5 mr-3 fa-2x" aria-hidden="true"></i>
-					</a>
-					<a class="tw-ic" href="/admin">
-						<i class="fa fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
-					</a>
-				</div>
-			</div>
-
-		</div>
-
-    </div>
-
-    <div class="footer-copyright text-center py-3">
+    <div class="footer-copyright text-center">
+		<a class="fb-ic" href="/faq">FAQ</a>&nbsp;-&nbsp;<a class="tw-ic" href="/admin">Admnistration</a>
+		<br/>
 		© 2018 Copyright - <a href="/"> hotelli.com</a>
     </div>
 
