205a913857a7057ffda35b36984c768a0f13527f
antonjnh <antonin.hebert@viacesi.fr>
2018-09-01 14:13:29 +0200

    Modification de la page d'accueil mise en place de la recherche

--- app/public/js/hotelli.js
+++ app/public/js/hotelli.js
@@ -1,18 +1,9 @@
 $(document).ready(function () {
-    console.log('ready');
     /**
      * Fonctionnement du login
      */
-    // envoi du formulaire
     $('#loginModalFormSubmit').click(function (event) {
         event.preventDefault();
         $('#loginModalForm').submit();
     });
-    //modal de confirmation
-    $('#infoLoginModal').modal('show');
-    setTimeout(doIt, 3000);
 });
-
-var closeInfoLogin = function () {
-    $('#infoLoginModal').modal('toggle');
-};
