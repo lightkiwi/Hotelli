5849aa610de4f6ebb08c1ad3a713ab7ed896770d
Emmanuel Bouchut <deepcraft@gmail.com>
2018-09-10 16:40:40 +0200

    prepa page hotel

---
+++ app/resources/views/layouts/working.blade.php
@@ -0,0 +1,13 @@
+<div class="opacity-article overflow-hidden page-wrap d-flex flex-row align-items-center bg-light">
+    <div class="col-md-12 mx-auto my-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-12 text-center">
+                    <div class="mb-4 lead">@lang('global.work_in_progress')</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+<br>
